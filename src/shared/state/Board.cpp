07a043ce378f8b7b0033db90bbc1e4ab755a2afc
Paul Viellet <viellet.p@gmail.com>
2019-10-18 17:08:50 +0000

    edit state.dia, King.cpp, Board.cpp
    preset value for King, code mod functions. start filling the table in Board, will be changed

--- src/shared/state/Board.cpp
+++ src/shared/state/Board.cpp
@@ -4,27 +4,39 @@ using namespace ::state;
 
 Board::Board() {
     //c'est totalement temporaire ... vous imaginez bien qu'on va pas laisser ça comme ça !
-//    for (int i; i < 13; i++) {
-//        for (int y; y < 13; y++) {
-//            tiles[i][y] = NULL;
-//        }
-//    }
-//    day = true;
-//
-//    int i, j;
-//    int t_edge1 = 0;
-//    int t_edge2 = 6;
-//    int t_edge3 = 12;
-//    for(i=t_edge1; i<t_edge2; i++){
-//
-//    }
+    this->day = true;
+
+    int i, j;
+    int t_edge1 = 0;
+    int t_edge2 = 6;
+    int t_edge3 = 12;
+
+    // set NULL spots in the tile table.
+    i = t_edge1;
+    j = t_edge2;
+    while(i != t_edge2+1) {
+        for(int k = t_edge1; k<j; k++) {
+            tiles[i][k] = nullptr;
+        }
+        j-=1;
+        i+=1;
+    }
+    i = t_edge3;
+    j = t_edge2;
+    while(i != t_edge2) {
+        for(int k = t_edge3; k>j; k--) {
+            tiles[i][k] = nullptr;
+        }
+        j+=1;
+        i-=1;
+    }
 }
 
 /**
  * Pass from day to night and vice versa
  */
 void Board::changeTime() {
-    day = !day;
+    this->day = !this->day;
 }
 
 /**
--- src/shared/state/King.cpp
+++ src/shared/state/King.cpp
@@ -4,3 +4,11 @@ using namespace ::state;
 King::King(state::Coordinate coordinate) : Tile(coordinate){
     this->path = "./../../../res/hexagon-pack/PNG/castle.png"
 }
+
+void modifyRot(){
+    this->rot -= 1;
+}
+
+void modifyLP(int offset){
+    this->lifePoints += offset;
+}
