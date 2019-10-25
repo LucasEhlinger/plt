c85187d694c04425502ee23cd036f67cb5d40825
Paul Viellet <viellet.p@gmail.com>
2019-10-25 17:07:06 +0000

    [add&edit] generate map, modify res resolution

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
@@ -15,15 +16,50 @@ using namespace std;
 
 int main(int argc,char* argv[])
 {
-    const char* forest_path = "/root/Documents/plt_true/res/hexagon-pack/PNG/forest.png";
-//    char castle_path = "/root/Documents/plt_true/res/hexagon-pack/PNG/castle.png";
-//    char swamp_path = "/root/Documents/plt_true/res/hexagon-pack/PNG/swamp.png";
-//    char ruin_path = "/root/Documents/plt_true/res/hexagon-pack/PNG/ruin.png";
-//    char stoneage_path = "/root/Documents/plt_true/res/hexagon-pack/PNG/stoneage.png";
+    int nb_row = 13;
+    int nb_col = 13;
+
+    const char* castle_path = "./../res/hexagon-pack/PNG/castle.png";
+    const char* colony_path = "./../res/hexagon-pack/PNG/colony.png";
+    const char* field_path = "./../res/hexagon-pack/PNG/field.png";
+    const char* forest_path = "./../res/hexagon-pack/PNG/forest.png";
+    const char* mountain_path = "./../res/hexagon-pack/PNG/mountain.png";
+    const char* ruin_path = "./../res/hexagon-pack/PNG/ruin.png";
+    const char* start_path = "./../res/hexagon-pack/PNG/start.png";
+    const char* stoneage_path = "./../res/hexagon-pack/PNG/stoneage.png";
+    const char* swamp_path = "./../res/hexagon-pack/PNG/swamp.png";
+
+    sf::Texture texCastle;
+    if(!texCastle.loadFromFile(castle_path))
+        return 0;
+    sf::Texture texColony;
+    if(!texColony.loadFromFile(colony_path))
+        return 0;
+    sf::Texture texField;
+    if(!texField.loadFromFile(field_path))
+        return 0;
+    sf::Texture texForest;
+    if(!texForest.loadFromFile(forest_path))
+        return 0;
+    sf::Texture texMountain;
+    if(!texMountain.loadFromFile(mountain_path))
+        return 0;
+    sf::Texture texRuin;
+    if(!texRuin.loadFromFile(ruin_path))
+        return 0;
+    sf::Texture texStart;
+    if(!texStart.loadFromFile(start_path))
+        return 0;
+    sf::Texture texStoneage;
+    if(!texStoneage.loadFromFile(stoneage_path))
+        return 0;
+    sf::Texture texSwamp;
+    if(!texSwamp.loadFromFile(swamp_path))
+        return 0;
 
     // create the window
-    sf::VideoMode vm(1024, 768);
-    sf::RenderWindow window(vm , "My window", sf::Style::Fullscreen); //sf::Style::Fullscreen or Default
+    sf::VideoMode vm(1280, 720);
+    sf::RenderWindow window(vm , "Such_plt_WOW", sf::Style::Close); //sf::Style::Fullscreen or Default
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -41,42 +77,52 @@ int main(int argc,char* argv[])
         window.clear(sf::Color::Black);
 
         // draw everything here...
-//        sf::IntRect ut_rec(-120,-140, 120, 140);
-//        sf::Texture texture;
-//        if(!texture.loadFromFile(, ut_rec))
-//            return 0;
-//        sf::Sprite sprite;
-//        sprite.setTexture(texture);
-//        window.draw(sprite);
-        // create an array of 3 vertices that define a hexa primitive
-        sf::VertexArray hexa(sf::TriangleFan, 8);
 
-        // define the position of the hexa's points
-        hexa[0].position = sf::Vector2f(60, 70);
-        hexa[1].position = sf::Vector2f(60, 0);
-        hexa[2].position = sf::Vector2f(0, 35);
-        hexa[3].position = sf::Vector2f(0, 105);
-        hexa[4].position = sf::Vector2f(60, 140);
-        hexa[5].position = sf::Vector2f(120, 105);
-        hexa[6].position = sf::Vector2f(120, 35);
-        hexa[7].position = sf::Vector2f(60, 0);
+        // create an array of 8 vertices that define a tile primitive
+        sf::VertexArray tile(sf::TriangleFan, 8);
 
-        // define texture area of hexa
-        hexa[0].texCoords = sf::Vector2f(60, 70);
-        hexa[1].texCoords = sf::Vector2f(60, 0);
-        hexa[2].texCoords = sf::Vector2f(0, 35);
-        hexa[3].texCoords = sf::Vector2f(0, 105);
-        hexa[4].texCoords = sf::Vector2f(60, 140);
-        hexa[5].texCoords = sf::Vector2f(120, 105);
-        hexa[6].texCoords = sf::Vector2f(120, 35);
-        hexa[7].texCoords = sf::Vector2f(60, 0);
+        // define texture area of tile
+        tile[0].texCoords = sf::Vector2f(36, 42);
+        tile[1].texCoords = sf::Vector2f(36, 0);
+        tile[2].texCoords = sf::Vector2f(0, 21);
+        tile[3].texCoords = sf::Vector2f(0, 63);
+        tile[4].texCoords = sf::Vector2f(36, 84);
+        tile[5].texCoords = sf::Vector2f(72, 63);
+        tile[6].texCoords = sf::Vector2f(72, 21);
+        tile[7].texCoords = sf::Vector2f(36, 0);
 
-        sf::Texture texture;
-        if(!texture.loadFromFile(forest_path))
-            return 0;
+        for(int i =  0; i<nb_row; ++i){
+            for(int j = 0; j<nb_col; ++j){
+                if(i%2 == 0){
+                    // define the position of the tile's points
+                    tile[0].position = sf::Vector2f(172+36+72*j, 0+42+63*i);
+                    tile[1].position = sf::Vector2f(172+36+72*j, 0+0+63*i);
+                    tile[2].position = sf::Vector2f(172+0+72*j, 0+21+63*i);
+                    tile[3].position = sf::Vector2f(172+0+72*j, 0+63+63*i);
+                    tile[4].position = sf::Vector2f(172+36+72*j, 0+84+63*i);
+                    tile[5].position = sf::Vector2f(172+72+72*j, 0+63+63*i);
+                    tile[6].position = sf::Vector2f(172+72+72*j, 0+21+63*i);
+                    tile[7].position = sf::Vector2f(172+36+72*j, 0+0+63*i);
 
-        // draw
-        window.draw(hexa, &texture);
+                    // draw
+                    window.draw(tile, &texCastle);
+                }
+                else{
+                    // define the position of the tile's points
+                    tile[0].position = sf::Vector2f(172+36+36+72*j, 0+63+42+63*(i-1));
+                    tile[1].position = sf::Vector2f(172+36+36+72*j, 0+63+0+63*(i-1));
+                    tile[2].position = sf::Vector2f(172+36+0+72*j, 0+63+21+63*(i-1));
+                    tile[3].position = sf::Vector2f(172+36+0+72*j, 0+63+63+63*(i-1));
+                    tile[4].position = sf::Vector2f(172+36+36+72*j, 0+63+84+63*(i-1));
+                    tile[5].position = sf::Vector2f(172+36+72+72*j, 0+63+63+63*(i-1));
+                    tile[6].position = sf::Vector2f(172+36+72+72*j, 0+63+21+63*(i-1));
+                    tile[7].position = sf::Vector2f(172+36+36+72*j, 0+63+0+63*(i-1));
+
+                    // draw
+                    window.draw(tile, &texMountain);
+                }
+            }
+        }
 
         // end the current frame
         window.display();
