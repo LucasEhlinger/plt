4262f2e5d31528f74bd3a3de3639b719f89799f8
Paul Viellet <viellet.p@gmail.com>
2019-11-07 23:05:09 +0000

    [edit] client/main.cpp
    now properly  generates a map out of a table of int

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -15,9 +15,112 @@ void testSFML() {
 
 using namespace std;
 
+// From sfml doc, turning verteces into entities, here a map
+class TileMap : public sf::Drawable, public sf::Transformable {
+public:
+
+    bool load(const std::string &tileset, sf::Vector2u tileSize, const int *tiles, unsigned int nb_col,
+              unsigned int nb_row) {
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Triangles);
+        m_vertices.resize(nb_col * nb_row * 3 * 4);
+
+        // populate the vertex array, with one quad per tile
+        for (unsigned int i = 0; i < nb_col; ++i) {
+            unsigned int odd_offset = 0;
+            for (unsigned int j = 0; j < nb_row; ++j) {
+                if (tiles[i * nb_row+ j] == 9)
+                    continue;
+                else {
+                    // get the current tile type
+                    int tileNumber = tiles[i *nb_row + j];
+
+                    // get a pointer to the current tile's quad
+                    sf::Vertex *hex = &m_vertices[(i + j * nb_col) * 3 * 4];
+
+                    if (i % 2 == 1)
+                        odd_offset = tileSize.x / 2;
+
+                    hex[0].position = sf::Vector2f(odd_offset + tileSize.x + tileSize.x * j,
+                                                   tileSize.y / 4 + tileSize.y * 3 / 4 * i);
+                    hex[1].position = sf::Vector2f(odd_offset + tileSize.x / 2 + tileSize.x * j,
+                                                   0 + tileSize.y * 3 / 4 * i);
+                    hex[2].position = sf::Vector2f(odd_offset + 0 + tileSize.x * j,
+                                                   tileSize.y / 4 + tileSize.y * 3 / 4 * i);
+                    hex[3].position = sf::Vector2f(odd_offset + 0 + tileSize.x * j,
+                                                   tileSize.y / 4 + tileSize.y * 3 / 4 * i);
+                    hex[4].position = sf::Vector2f(odd_offset + 0 + tileSize.x * j,
+                                                   tileSize.y * 3 / 4 + tileSize.y * 3 / 4 * i);
+                    hex[5].position = sf::Vector2f(odd_offset + tileSize.x / 2 + tileSize.x * j,
+                                                   tileSize.y + tileSize.y * 3 / 4 * i);
+                    hex[6].position = sf::Vector2f(odd_offset + tileSize.x / 2 + tileSize.x * j,
+                                                   tileSize.y + tileSize.y * 3 / 4 * i);
+                    hex[7].position = sf::Vector2f(odd_offset + tileSize.x + tileSize.x * j,
+                                                   tileSize.y * 3 / 4 + tileSize.y * 3 / 4 * i);
+                    hex[8].position = sf::Vector2f(odd_offset + tileSize.x + tileSize.x * j,
+                                                   tileSize.y / 4 + tileSize.y * 3 / 4 * i);
+                    hex[9].position = sf::Vector2f(odd_offset + tileSize.x + tileSize.x * j,
+                                                   tileSize.y / 4 + tileSize.y * 3 / 4 * i);
+                    hex[10].position = sf::Vector2f(odd_offset + 0 + tileSize.x * j,
+                                                    tileSize.y / 4 + tileSize.y * 3 / 4 * i);
+                    hex[11].position = sf::Vector2f(odd_offset + tileSize.x / 2 + tileSize.x * j,
+                                                    tileSize.y + tileSize.y * 3 / 4 * i);
+
+                    hex[0].texCoords = sf::Vector2f(tileSize.x + tileSize.x * tileNumber,
+                                                   tileSize.y / 4);
+                    hex[1].texCoords = sf::Vector2f(tileSize.x / 2 + tileSize.x * tileNumber,
+                                                   0);
+                    hex[2].texCoords = sf::Vector2f(0 + tileSize.x * tileNumber,
+                                                   tileSize.y / 4);
+                    hex[3].texCoords = sf::Vector2f(0 + tileSize.x * tileNumber,
+                                                   tileSize.y / 4);
+                    hex[4].texCoords = sf::Vector2f(0 + tileSize.x * tileNumber,
+                                                   tileSize.y * 3 / 4);
+                    hex[5].texCoords = sf::Vector2f(tileSize.x / 2 + tileSize.x * tileNumber,
+                                                   tileSize.y);
+                    hex[6].texCoords = sf::Vector2f(tileSize.x / 2 + tileSize.x * tileNumber,
+                                                   tileSize.y);
+                    hex[7].texCoords = sf::Vector2f(tileSize.x + tileSize.x * tileNumber,
+                                                   tileSize.y * 3 / 4);
+                    hex[8].texCoords = sf::Vector2f(tileSize.x + tileSize.x * tileNumber,
+                                                   tileSize.y / 4);
+                    hex[9].texCoords = sf::Vector2f(tileSize.x + tileSize.x * tileNumber,
+                                                   tileSize.y / 4);
+                    hex[10].texCoords = sf::Vector2f(0 + tileSize.x * tileNumber,
+                                                    tileSize.y / 4);
+                    hex[11].texCoords = sf::Vector2f(tileSize.x / 2 + tileSize.x * tileNumber,
+                                                    tileSize.y);
+                }
+            }
+        }
+        return true;
+    }
+
+private:
+
+    virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
+
 int testRender();
 
 int main(int argc, char *argv[]) {
+    testRender();
     if (argc == 2) {
         if (strcmp(argv[1], "hello") == 0) {
             cout << "Bonjour à tous !" << endl;
@@ -34,162 +137,38 @@ int main(int argc, char *argv[]) {
 }
 
 int testRender() {
-    const int screen_width = 1536;
-    const int screen_height = 860;
-    const int nb_row = 13;
-    const int nb_col = nb_row;
-    const int middle_row = (nb_row + nb_row % 2) / 2;
-    const int off_mar_X = 172;
-    const int off_mar_Y = 10;
-    const int tile_w = 72;
-    const int tile_h = 84;
-    const int shift_X = tile_w / 2;
-    const int shift_Y = tile_h * 3 / 4;
-    const int ite_row = tile_w;
-    const int ite_col = tile_h * 3 / 4;
-
-    const char *castle_path = "./../res/hexagon-pack/PNG/castle.png";
-    const char *colony_path = "./../res/hexagon-pack/PNG/colony.png";
-    const char *field_path = "./../res/hexagon-pack/PNG/field.png";
-    const char *forest_path = "./../res/hexagon-pack/PNG/forest.png";
-    const char *mountain_path = "./../res/hexagon-pack/PNG/mountain.png";
-    const char *ruin_path = "./../res/hexagon-pack/PNG/ruin.png";
-    const char *start_path = "./../res/hexagon-pack/PNG/start.png";
-    const char *stoneage_path = "./../res/hexagon-pack/PNG/stoneage.png";
-    const char *swamp_path = "./../res/hexagon-pack/PNG/swamp.png";
-
-    sf::Texture texCastle;
-    if (!texCastle.loadFromFile(castle_path))
-        return 0;
-    sf::Texture texColony;
-    if (!texColony.loadFromFile(colony_path))
-        return 0;
-    sf::Texture texField;
-    if (!texField.loadFromFile(field_path))
-        return 0;
-    sf::Texture texForest;
-    if (!texForest.loadFromFile(forest_path))
-        return 0;
-    sf::Texture texMountain;
-    if (!texMountain.loadFromFile(mountain_path))
-        return 0;
-    sf::Texture texRuin;
-    if (!texRuin.loadFromFile(ruin_path))
-        return 0;
-    sf::Texture texStart;
-    if (!texStart.loadFromFile(start_path))
-        return 0;
-    sf::Texture texStoneage;
-    if (!texStoneage.loadFromFile(stoneage_path))
-        return 0;
-    sf::Texture texSwamp;
-    if (!texSwamp.loadFromFile(swamp_path))
-        return 0;
 
     // create the window
-    sf::VideoMode vm(screen_width, screen_height);
-    sf::RenderWindow window(vm, "Such_plt_WOW", sf::Style::Close); //sf::Style::Fullscreen or Default
+    sf::RenderWindow window(sf::VideoMode(1536, 860), "Tilemap");
 
-    // run the program as long as the window is open
+    // define the level with an array of tile indices
+    const int level[] =
+            {
+                    0, 0, 0, 0,
+                    1, 1, 1, 1,
+                    2, 2, 2, 2,
+                    3, 3, 3, 3
+            };
+
+    // create the tilemap from the level definition
+    TileMap map;
+    if (!map.load("./../res/hexagon-pack/PNG/tileset.png", sf::Vector2u(72, 84), level, 4, 4))
+        return -1;
+
+    // run the main loop
     while (window.isOpen()) {
-        // check all the window's events that were triggered since the last iteration of the loop
+        // handle events
         sf::Event event;
         while (window.pollEvent(event)) {
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-
-        // create an array of 8 vertices that define a tile primitive
-        sf::VertexArray tile(sf::TriangleFan, 8);
-
-        // define texture area of tile
-        /* A bit tricky, before defining the shape of the vertex, we set what will be the 'texturabale' area,
-        without knowing if it will fit in the vertex. We assume that we know what we are doing and that at worst
-        this area can be wider, the texture won't leak. */
-        tile[0].texCoords = sf::Vector2f(tile_w / 2, tile_h / 2);
-        tile[1].texCoords = sf::Vector2f(tile_w / 2, 0);
-        tile[2].texCoords = sf::Vector2f(0, tile_h / 4);
-        tile[3].texCoords = sf::Vector2f(0, tile_h * 3 / 4);
-        tile[4].texCoords = sf::Vector2f(tile_w / 2, tile_h);
-        tile[5].texCoords = sf::Vector2f(tile_w, tile_h * 3 / 4);
-        tile[6].texCoords = sf::Vector2f(tile_w, tile_h / 4);
-        tile[7].texCoords = sf::Vector2f(tile_w / 2, 0);
-
-        int nb_tiles;
-        for (int i = 0; i < nb_row; ++i) {
-            int odd_offset = 0;
-            nb_tiles = nb_col - abs(middle_row - 1 - i);
-            if (nb_tiles % 2 == 1) {
-                // define the position of the tile's points
-                // If odd number of tiles, we have to put a tile in the middle of the line
-                tile[0].position = sf::Vector2f(screen_width / 2, off_mar_Y + tile_h / 2 + ite_col * i);
-                tile[1].position = sf::Vector2f(screen_width / 2, off_mar_Y + 0 + ite_col * i);
-                tile[2].position = sf::Vector2f(screen_width / 2 - tile_w / 2,
-                                                off_mar_Y + tile_h / 4 + ite_col * i);
-                tile[3].position = sf::Vector2f(screen_width / 2 - tile_w / 2,
-                                                off_mar_Y + tile_h * 3 / 4 + ite_col * i);
-                tile[4].position = sf::Vector2f(screen_width / 2, off_mar_Y + tile_h + ite_col * i);
-                tile[5].position = sf::Vector2f(screen_width / 2 + tile_w / 2,
-                                                off_mar_Y + tile_h * 3 / 4 + ite_col * i);
-                tile[6].position = sf::Vector2f(screen_width / 2 + tile_w / 2,
-                                                off_mar_Y + tile_h / 4 + ite_col * i);
-                tile[7].position = sf::Vector2f(screen_width / 2, off_mar_Y + 0 + ite_col * i);
-                // draw
-                window.draw(tile, &texCastle);
-
-                odd_offset = tile_w / 2;
-                --nb_tiles;
-            }
-            for (int j = 0; j < nb_tiles / 2; ++j) {
-                tile[0].position = sf::Vector2f(screen_width / 2 + odd_offset + tile_w / 2 + ite_row * j,
-                                                off_mar_Y + tile_h / 2 + ite_col * i);
-                tile[1].position = sf::Vector2f(screen_width / 2 + odd_offset + tile_w / 2 + ite_row * j,
-                                                off_mar_Y + 0 + ite_col * i);
-                tile[2].position = sf::Vector2f(screen_width / 2 + odd_offset + 0 + ite_row * j,
-                                                off_mar_Y + tile_h / 4 + ite_col * i);
-                tile[3].position = sf::Vector2f(screen_width / 2 + odd_offset + 0 + ite_row * j,
-                                                off_mar_Y + tile_h * 3 / 4 + ite_col * i);
-                tile[4].position = sf::Vector2f(screen_width / 2 + odd_offset + tile_w / 2 + ite_row * j,
-                                                off_mar_Y + tile_h + ite_col * i);
-                tile[5].position = sf::Vector2f(screen_width / 2 + odd_offset + tile_w + ite_row * j,
-                                                off_mar_Y + tile_h * 3 / 4 + ite_col * i);
-                tile[6].position = sf::Vector2f(screen_width / 2 + odd_offset + tile_w + ite_row * j,
-                                                off_mar_Y + tile_h / 4 + ite_col * i);
-                tile[7].position = sf::Vector2f(screen_width / 2 + odd_offset + tile_w / 2 + ite_row * j,
-                                                off_mar_Y + 0 + ite_col * i);
-                // draw
-                window.draw(tile, &texField);
-            }
-            for (int j = 0; j < nb_tiles / 2; ++j) {
-                tile[0].position = sf::Vector2f(screen_width / 2 - odd_offset + tile_w / 2 - ite_row * (j + 1),
-                                                off_mar_Y + tile_h / 2 + ite_col * i);
-                tile[1].position = sf::Vector2f(screen_width / 2 - odd_offset + tile_w / 2 - ite_row * (j + 1),
-                                                off_mar_Y + 0 + ite_col * i);
-                tile[2].position = sf::Vector2f(screen_width / 2 - odd_offset + 0 - ite_row * (j + 1),
-                                                off_mar_Y + tile_h / 4 + ite_col * i);
-                tile[3].position = sf::Vector2f(screen_width / 2 - odd_offset + 0 - ite_row * (j + 1),
-                                                off_mar_Y + tile_h * 3 / 4 + ite_col * i);
-                tile[4].position = sf::Vector2f(screen_width / 2 - odd_offset + tile_w / 2 - ite_row * (j + 1),
-                                                off_mar_Y + tile_h + ite_col * i);
-                tile[5].position = sf::Vector2f(screen_width / 2 - odd_offset + tile_w - ite_row * (j + 1),
-                                                off_mar_Y + tile_h * 3 / 4 + ite_col * i);
-                tile[6].position = sf::Vector2f(screen_width / 2 - odd_offset + tile_w - ite_row * (j + 1),
-                                                off_mar_Y + tile_h / 4 + ite_col * i);
-                tile[7].position = sf::Vector2f(screen_width / 2 - odd_offset + tile_w / 2 - ite_row * (j + 1),
-                                                off_mar_Y + 0 + ite_col * i);
-                // draw
-                window.draw(tile, &texSwamp);
-            }
-        }
-
-        // end the current frame
+        // draw the map
+        window.clear();
+        window.draw(map);
         window.display();
     }
+
     return 0;
 }
