4b2994faf9f9c0f4bbc1c1fafbc247e0a5e9a5fd
Lucas Ehlinger <lucas.ehlinger@gmail.com>
2019-11-11 21:18:46 +0100

    [add] test failed load TileMap and test drawing

--- test/client/test_client_tileMap.cpp
+++ test/client/test_client_tileMap.cpp
@@ -12,40 +12,127 @@ using namespace std;
 
 
 BOOST_AUTO_TEST_CASE(TestLoad) {
-    const int screen_width = 1536;
-    const int screen_height = 860;
-    const int tile_width = 72;
-    const int tile_height = 84;
-    const int nb_col = 13;
-    const int nb_row = 13;
-    unsigned int tiles[nb_col * nb_row];
-    unsigned int level[nb_col * nb_row];
+    {
+        const int screen_width = 1536;
+        const int screen_height = 860;
+        const int tile_width = 72;
+        const int tile_height = 84;
+        const int nb_col = 13;
+        const int nb_row = 13;
+        unsigned int tiles[nb_col * nb_row];
+        unsigned int level[nb_col * nb_row];
 
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "just_another_plt_map");
+        // create the window
+        sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "just_another_plt_map");
 
-    render::Scene scene{};
-    scene.draw(tiles);
-    for (int i = 0; i < nb_row; ++i) {
-        int blanks = abs((nb_row - 1) / 2 - i);
-        int blanks_even = blanks % 2;
-        for (int j = 0; j < nb_col; ++j) {
-            if (j < (blanks + blanks_even) / 2)
-                level[i * nb_row + j] = 9;
-            else if (j >= nb_col - blanks + (blanks + blanks_even) / 2)
-                level[i * nb_row + j] = 9;
-            else if (i < (nb_row - 1) / 2)
-                level[i * nb_row + j] = tiles[i * nb_row + j + (blanks - blanks_even) / 2];
-            else
-                level[i * nb_row + j] = tiles[i * nb_row + j - (blanks + blanks_even) / 2];
+        render::Scene scene{};
+        scene.draw(tiles);
+        for (int i = 0; i < nb_row; ++i) {
+            int blanks = abs((nb_row - 1) / 2 - i);
+            int blanks_even = blanks % 2;
+            for (int j = 0; j < nb_col; ++j) {
+                if (j < (blanks + blanks_even) / 2)
+                    level[i * nb_row + j] = 9;
+                else if (j >= nb_col - blanks + (blanks + blanks_even) / 2)
+                    level[i * nb_row + j] = 9;
+                else if (i < (nb_row - 1) / 2)
+                    level[i * nb_row + j] = tiles[i * nb_row + j + (blanks - blanks_even) / 2];
+                else
+                    level[i * nb_row + j] = tiles[i * nb_row + j - (blanks + blanks_even) / 2];
+            }
         }
+
+        // create the tilemap from the level definition
+        render::TileMap map;
+        //"./../res/hexagon-pack/PNG/tileset.png"
+        BOOST_CHECK(map.load("../../../res/hexagon-pack/PNG/tileset.png", sf::Vector2u(tile_width, tile_height), level,
+                             nb_col,
+                             nb_row));
+
     }
+    {
+        const int screen_width = 1536;
+        const int screen_height = 860;
+        const int tile_width = 72;
+        const int tile_height = 84;
+        const int nb_col = 13;
+        const int nb_row = 13;
+        unsigned int tiles[nb_col * nb_row];
+        unsigned int level[nb_col * nb_row];
 
-    // create the tilemap from the level definition
-    render::TileMap map;
-    //"./../res/hexagon-pack/PNG/tileset.png"
-    BOOST_CHECK(map.load("../../../res/hexagon-pack/PNG/tileset.png", sf::Vector2u(tile_width, tile_height), level, nb_col,
-                         nb_row));
+        // create the window
+        sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "just_another_plt_map");
+
+        render::Scene scene{};
+        scene.draw(tiles);
+        for (int i = 0; i < nb_row; ++i) {
+            int blanks = abs((nb_row - 1) / 2 - i);
+            int blanks_even = blanks % 2;
+            for (int j = 0; j < nb_col; ++j) {
+                if (j < (blanks + blanks_even) / 2)
+                    level[i * nb_row + j] = 9;
+                else if (j >= nb_col - blanks + (blanks + blanks_even) / 2)
+                    level[i * nb_row + j] = 9;
+                else if (i < (nb_row - 1) / 2)
+                    level[i * nb_row + j] = tiles[i * nb_row + j + (blanks - blanks_even) / 2];
+                else
+                    level[i * nb_row + j] = tiles[i * nb_row + j - (blanks + blanks_even) / 2];
+            }
+        }
+
+        // create the tilemap from the level definition
+        render::TileMap map;
+        //"./../res/hexagon-pack/PNG/tileset.png"
+        BOOST_CHECK(map.load("../../../res/hexagon-pack/PNG/tileset.png", sf::Vector2u(tile_width, tile_height), level,
+                             nb_col,
+                             nb_row));
+        
+        while (window.isOpen()){
+            window.clear();
+            window.draw(map);
+            window.close();
+        }
+
+    }
+}
+BOOST_AUTO_TEST_CASE(TestFailLoad){
+    {
+        const int screen_width = 1536;
+        const int screen_height = 860;
+        const int tile_width = 72;
+        const int tile_height = 84;
+        const int nb_col = 13;
+        const int nb_row = 13;
+        unsigned int tiles[nb_col * nb_row];
+        unsigned int level[nb_col * nb_row];
+
+        // create the window
+        sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "just_another_plt_map");
+
+        render::Scene scene{};
+        scene.draw(tiles);
+        for (int i = 0; i < nb_row; ++i) {
+            int blanks = abs((nb_row - 1) / 2 - i);
+            int blanks_even = blanks % 2;
+            for (int j = 0; j < nb_col; ++j) {
+                if (j < (blanks + blanks_even) / 2)
+                    level[i * nb_row + j] = 9;
+                else if (j >= nb_col - blanks + (blanks + blanks_even) / 2)
+                    level[i * nb_row + j] = 9;
+                else if (i < (nb_row - 1) / 2)
+                    level[i * nb_row + j] = tiles[i * nb_row + j + (blanks - blanks_even) / 2];
+                else
+                    level[i * nb_row + j] = tiles[i * nb_row + j - (blanks + blanks_even) / 2];
+            }
+        }
+
+        // create the tilemap from the level definition
+        render::TileMap map;
+        //"./../res/hexagon-pack/PNG/tileset.png"
+        BOOST_CHECK(!map.load("../../res/hexagon-pack/PNG/tileset.png", sf::Vector2u(tile_width, tile_height), level,
+                              nb_col,
+                              nb_row));
 
 
+    }
 }
