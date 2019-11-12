4e6469f5f3d91f0577003fd954926318fe25bb6d
Lucas Ehlinger <lucas.ehlinger@gmail.com>
2019-11-12 11:20:39 +0100

    [edit] engine pawn move

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -6,8 +6,10 @@ Engine::Engine(state::Board& board) : board(board){}
 
 void Engine::move(state::Pawn &pawn, state::Coordinate to){
     int position = to.getCoordInLine();
-    if (board.tiles.at(position) != NULL)
+    if (board.tiles.at(position) != NULL || (pawn.getAP() + board.tiles.at(position)->getMoveCost()) >= 0)
     {
         pawn.move(to);
+        board.tiles.at(position)->effect(pawn);
+        pawn.notify();
     }
 }
