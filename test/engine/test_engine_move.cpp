347882a214c15b20ed07f89136430c91670e35ce
Lucas Ehlinger <lucas.ehlinger@gmail.com>
2019-11-13 19:54:23 +0100

    [add] improve test coverage

--- test/engine/test_engine_move.cpp
+++ test/engine/test_engine_move.cpp
@@ -10,8 +10,12 @@ BOOST_AUTO_TEST_CASE(TestMove){
     {
         Board bo{};
         bo.generate();
-
+        bo.pawns.push_back(Player{Coordinate{0,6}, "player1"});
         Engine en{bo};
+        en.move(bo.pawns.front(), Coordinate{1,7});
+
+        BOOST_CHECK(bo.pawns.front().getCoordinate() == en.board->pawns.front().getCoordinate());
+
 
     }
 }
