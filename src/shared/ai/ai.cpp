ac1ba6d29b9ce24496bd3ed5663b88adaeab8d11
Lucas Ehlinger <lucas.ehlinger@gmail.com>
2019-11-20 23:24:37 +0100

    [fix] rand lib for Ai classe

--- src/shared/ai/ai.cpp
+++ src/shared/ai/ai.cpp
@@ -1,7 +1,10 @@
-#include "ai.h"
+#include "Ai.h"
+#include <cmath>
+
 
 using namespace ai;
 
 state::Coordinate Ai::AI_rand(std::vector<state::Coordinate> av_moves) {
+    srand(time(NULL));
     return av_moves[rand() % av_moves.size()];
 }
