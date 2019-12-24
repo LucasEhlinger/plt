3dc4470cdf47b72be332d824abbfa355af891974
Paul Viellet <viellet.p@gmail.com>
2019-12-24 13:56:55 +0000

    [fix] correct distance computation
    Also fixed a minor issue due to movecosts being inherently negatives. Last thing is to determine what the function is supposed to return :^)

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -32,6 +32,9 @@ Engine::Engine(state::Board &board) : board(&board) {
 }
 
 void sim_attack(state::Pawn playing, state::Coordinate goal, bool aim, int modifier = 0);
+sf::Vector3i evenr_to_cube(state::Coordinate hex);
+int offset_distance(state::Coordinate hex_a, state::Coordinate hex_b);
+int cube_distance(sf::Vector3i hex_a, sf::Vector3i hex_b);
 
 void Engine::move(state::Pawn &pawn, state::Coordinate to) {
     int position = to.getCoordInLine();
@@ -131,10 +134,10 @@ state::Coordinate Engine::pathfinding(state::Coordinate goal) {
         Engine::playingPawn().setAP(3);
         neighbors = Engine::matrixAv_Tile(Engine::playingPawn());
         for (state::Coordinate next : neighbors) {
-            int new_cost = cost_so_far[current] + board->tiles.at(next.getCoordInLine()).getMoveCost();
+            int new_cost = cost_so_far[current] - board->tiles.at(next.getCoordInLine()).getMoveCost();
             if (cost_so_far.find(next.getCoordInLine()) == cost_so_far.end() || new_cost < cost_so_far[next.getCoordInLine()]) {
                 cost_so_far[next.getCoordInLine()] = new_cost;
-                int priority = new_cost + std::abs(goal.getRow() - cur_coor.getRow() + goal.getColumn() - cur_coor.getColumn());
+                int priority = new_cost + offset_distance(cur_coor, goal);
                 frontier.put(next.getCoordInLine(), priority);
                 came_from[next.getCoordInLine()] = current;
             }
@@ -143,6 +146,23 @@ state::Coordinate Engine::pathfinding(state::Coordinate goal) {
     Engine::playingPawn().setCoordinate(start);
     Engine::playingPawn().setAP(save);
     return start;
+}
+
+sf::Vector3i evenr_to_cube(state::Coordinate hex) {
+    int x = hex.getColumn() - (hex.getRow() + (hex.getRow() & 1)) / 2;
+    int z = hex.getRow();
+    int y = -x - z;
+    return sf::Vector3i{x, y, z};
+}
+
+int offset_distance(state::Coordinate hex_a, state::Coordinate hex_b) {
+    sf::Vector3i hex_a_c = evenr_to_cube(hex_a);
+    sf::Vector3i hex_b_c = evenr_to_cube(hex_b);
+    return cube_distance(hex_a_c, hex_b_c);
+}
+
+int cube_distance(sf::Vector3i hex_a, sf::Vector3i hex_b) {
+    return std::max(std::max(std::abs(hex_a.x - hex_b.x), std::abs(hex_a.y - hex_b.y)), std::abs(hex_a.z - hex_b.z));
 }
 
 int Engine::attack(state::Coordinate to) {
